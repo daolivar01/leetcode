4e23b066e2afd7395e0b6ccc4b2b36d94a685446
David Olivares <davidswe03@gmail.com>
2025-04-28 22:31:43 -0700

    Adds Python solution for Reorder List

---
+++ python/reorder_list.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return  # No need to reorder if list has 0 or 1 node
+
+        # Step 1: Find the middle of the list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half of the list
+        prev, curr = None, slow.next
+        slow.next = None  # Split the list into two halves
+        while curr:
+            next_temp = curr.next
+            curr.next = prev  # Reverse pointer
+            prev = curr
+            curr = next_temp
+
+        # Step 3: Merge the two halves
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
